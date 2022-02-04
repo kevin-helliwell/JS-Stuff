fa44a954a9a0c5e3c38a4166439227cdab095a49
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-03 22:23:56 -0500

    update

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -282,44 +282,73 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isInteger(23 / 0)); // false
 
 // Math & Rounding
-console.log(Math.sqrt(25)); // 5
-console.log(25 ** (1 / 2)); // 5
-console.log(8 ** (1 / 3)); //  2
+// console.log(Math.sqrt(25)); // 5
+// console.log(25 ** (1 / 2)); // 5
+// console.log(8 ** (1 / 3)); //  2
 
-console.log(Math.max(5, 18, 23, 11, 2)); // 23
-console.log(Math.max(5, 18, '23', 11, 2)); // 23
-console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+// console.log(Math.max(5, 18, 23, 11, 2)); // 23
+// console.log(Math.max(5, 18, '23', 11, 2)); // 23
+// console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
 
-console.log(Math.min(5, 18, 23, 11, 2)); // 2
+// console.log(Math.min(5, 18, 23, 11, 2)); // 2
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.159265...
+// console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.159265...
 
-console.log(Math.trunc(Math.random() * 6) + 1); // random int 0-6
+// console.log(Math.trunc(Math.random() * 6) + 1); // random int 0-6
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
-// 0...1 => 0...(max-min) => min...max
-console.log(randomInt(10, 20)); // random number between 10 and 20
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+// // 0...1 => 0...(max-min) => min...max
+// console.log(randomInt(10, 20)); // random number between 10 and 20
 
-// Rounding integers
+// // Rounding integers
 
-console.log(Math.round(23.3)); // 23
-console.log(Math.round(23.9)); // 24
+// console.log(Math.round(23.3)); // 23
+// console.log(Math.round(23.9)); // 24
 
-console.log(Math.ceil(23.3)); // 24
-console.log(Math.ceil(23.9)); // 24
+// console.log(Math.ceil(23.3)); // 24
+// console.log(Math.ceil(23.9)); // 24
 
-console.log(Math.floor(23.3)); // 23
-console.log(Math.floor('23.9')); // 23 (number, .floor does type coercion)
+// console.log(Math.floor(23.3)); // 23
+// console.log(Math.floor('23.9')); // 23 (number, .floor does type coercion)
 
-console.log(Math.trunc(23.3)); // 23
+// console.log(Math.trunc(23.3)); // 23
 
-console.log(Math.trunc(-23.3)); // -23
-console.log(Math.floor(-23.3)); // -24
+// console.log(Math.trunc(-23.3)); // -23
+// console.log(Math.floor(-23.3)); // -24
 
-// Rounding decimals
-console.log((2.7).toFixed(0)); // 3 (always returns a string)
-console.log((2.7).toFixed(3)); // 2.700 (string)
-console.log((2.345).toFixed(2)); // 2.35 (string)
-console.log(+(2.345).toFixed(2)); // 2.35 (number)
+// // Rounding decimals
+// console.log((2.7).toFixed(0)); // 3 (always returns a string)
+// console.log((2.7).toFixed(3)); // 2.700 (string)
+// console.log((2.345).toFixed(2)); // 2.35 (string)
+// console.log(+(2.345).toFixed(2)); // 2.35 (number)
 
 // The Remainder Operator
+// console.log(5 % 2); // 1
+// console.log(5 / 2); // 5 = 2 * 2 + 1
+
+// console.log(8 % 3); // 2
+// console.log(8 / 3); // 8 = 2 * 3 + 2
+
+// console.log(6 % 2); // 0
+// console.log(6 / 2); // 3
+
+// console.log(7 % 2); // 1
+// console.log(7 / 2); // 3.5
+
+// const isEven = (n) => n % 2 === 0;
+// console.log(isEven(8)); // true
+// console.log(isEven(23)); // false
+// console.log(isEven(514)); // true
+
+// Makes even rows orangred and multiples of 3 blue
+
+labelBalance.addEventListener('click', function () {
+    [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+        // 0, 2, 4, 6, ...
+        if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+        // 0, 3, 6, 9, ...
+        if (i % 3 === 0) row.style.backgroundColor = 'blue';
+    });
+});
+
+// Numeric Separators
