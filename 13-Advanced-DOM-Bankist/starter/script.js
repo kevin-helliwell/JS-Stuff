b00828e03291662f1cd000f92471671756934fa6
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-11 00:13:46 -0500

    update

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -128,4 +128,69 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
     const s1coords = section1.getBoundingClientRect();
     console.log(s1coords);
+
+    console.log(e.target.getBoundingClientRect());
+
+    console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+    console.log(
+        'height/width viewport',
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth
+    );
+
+    // // Scrolling
+    // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset); // Current Position + Current Scroll
+
+    // Old-school way
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth',
+    // });
+    // Modern way
+    section1.scrollIntoView({behavior: 'smooth'}); // only works in modern browsers
 });
+
+// // Types of events and event handlers
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//     alert('addEventListener: Great! You are reading the heading :D');
+// };
+// // Usual way
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// // Old way
+// // h1.onmouseenter = function (e) {
+// //     alert('addEventListener: Great! You are reading the heading :D');
+// // };
+
+// Event propagation in practice
+// rgb(0-255,0-255,0-255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('LINK', e.target, e.currentTarget);
+//     console.log(e.currentTarget === this); // true
+
+//     // Stop propagation
+//     // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('CONTAINER', e.target, e.currentTarget);
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('NAV', e.target, e.currentTarget);
+// });
+
+// Event delegation: implementing page navigation
