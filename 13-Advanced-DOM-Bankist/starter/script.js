a7f05dfba1ee190032a613f5bef2e7719f1b3622
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-12 20:34:02 -0500

    update

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,19 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
+
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.btn--close-modal');
+// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -30,6 +37,44 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
+    }
+});
+
+// // Scrolling
+// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset); // Current Position + Current Scroll
+
+// Old-school way
+// window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+// });
+// Modern way
+section1.scrollIntoView({behavior: 'smooth'}); // only works in modern browsers
+
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const id = this.getAttribute('href');
+//         console.log(id);
+//         document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//     });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine which element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        console.log(id);
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+        // console.log('LINK');
     }
 });
 
@@ -122,9 +167,10 @@ document.addEventListener('keydown', function (e) {
 // Don't use (overrides existing classes, only one class)
 // logo.className = 'jonas'
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
+// Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
     const s1coords = section1.getBoundingClientRect();
     console.log(s1coords);
@@ -149,7 +195,7 @@ btnScrollTo.addEventListener('click', function (e) {
     //     behavior: 'smooth',
     // });
     // Modern way
-    section1.scrollIntoView({behavior: 'smooth'}); // only works in modern browsers
+    // section1.scrollIntoView({behavior: 'smooth'}); // only works in modern browsers
 });
 
 // // Types of events and event handlers
