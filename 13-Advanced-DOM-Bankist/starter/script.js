2185d3f362f9599211e0888c7cc45567e54fd51e
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-13 15:06:38 -0500

    reveal sections on scroll update

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -178,7 +178,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
     const [entry] = entries;
-    console.log(entry);
+    // console.log(entry);
 
     if (!entry.isIntersecting) return;
 
