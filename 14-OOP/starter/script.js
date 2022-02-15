88ae93a3fbedd1f9f45b7b93c29d1ff647024ce2
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-14 22:35:21 -0500

    update constructors and objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+// convention: constructor variables capitalized
+// can't use arrow function since no this keyword
+const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. new {} is created
+// 2. function is called
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); // true
