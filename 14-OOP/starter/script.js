64f0afb4e4b8d008cacb7f959dad8e9fb41ae3df
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-17 18:24:52 -0500

    inheritance between classes update

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -261,4 +261,40 @@ GOOD LUCK 😀
 // ford.speedUS = 50;
 // console.log(ford);
 
-// Inheritance between classes
+// Inheritance Between "Classes": Constructor Functions
+
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+// const Student = function (firstName, birthYear, course) {
+//     Person.call(this, firstName, birthYear);
+//     this.course = course;
+// };
+
+// // Linking prototypes
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//     console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// const mike = new Student('Mike', 2020, 'Computer Science');
+// console.log(mike);
+// mike.introduce();
+// mike.calcAge();
+
+// console.log(mike.__proto__);
+// console.log(mike.__proto__.__proto__);
+
+// console.log(mike instanceof Student); // true
+// console.log(mike instanceof Person); // true
+// console.log(mike instanceof Object); // true
+
+// Student.prototype.constructor = Student;
+// console.dir(Student.prototype.constructor);
