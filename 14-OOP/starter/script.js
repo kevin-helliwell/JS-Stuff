1a57af14bbad0bd1b3ec614faf16897c47d0f18f
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-16 20:49:43 -0500

    object.create update

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -119,41 +119,41 @@ GOOD LUCK 😀
 // const PersonCL = class {};
 
 // // Class declaration
-class PersonCl {
-    constructor(fullName, birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
-    }
-    // Instance methods
-    // Methods will be added to .prototype property
-    calcAge() {
-        console.log(2037 - this.birthYear);
-    }
-    greet() {
-        console.log(`Hey ${this.fullName}`);
-    }
+// class PersonCl {
+//     constructor(fullName, birthYear) {
+//         this.fullName = fullName;
+//         this.birthYear = birthYear;
+//     }
+//     // Instance methods
+//     // Methods will be added to .prototype property
+//     calcAge() {
+//         console.log(2037 - this.birthYear);
+//     }
+//     greet() {
+//         console.log(`Hey ${this.fullName}`);
+//     }
 
-    get age() {
-        return 2037 - this.birthYear;
-    }
+//     get age() {
+//         return 2037 - this.birthYear;
+//     }
 
-    // Set a property that already exists
-    set fullName(name) {
-        console.log(name);
-        if (name.includes(' ')) this._fullName = name;
-        else alert(`${name} is not a full name!`);
-    }
+//     // Set a property that already exists
+//     set fullName(name) {
+//         console.log(name);
+//         if (name.includes(' ')) this._fullName = name;
+//         else alert(`${name} is not a full name!`);
+//     }
 
-    get fullName() {
-        return this._fullName;
-    }
+//     get fullName() {
+//         return this._fullName;
+//     }
 
-    // Static method
-    static hey() {
-        console.log(`Hey there👋`);
-        console.log(this);
-    }
-}
+//     // Static method
+//     static hey() {
+//         console.log(`Hey there👋`);
+//         console.log(this);
+//     }
+// }
 
 // const jessica = new PersonCl('Jessica Davis', 1996);
 // console.log(jessica);
@@ -193,3 +193,28 @@ class PersonCl {
 
 // account.latest = 50;
 // console.log(account.movements);
+
+// Object.create
+
+// const PersonProto = {
+//     calcAge() {
+//         console.log(2037 - this.birthYear);
+//     },
+
+//     init(firstName, birthYear) {
+//         this.firstName = firstName;
+//         this.birthYear = birthYear;
+//     },
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
+// steven.calcAge();
+
+// console.log(steven.__proto__);
+
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
