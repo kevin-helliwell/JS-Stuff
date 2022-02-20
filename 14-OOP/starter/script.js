b5a72624231bcab354a7001e7daf5400763e3073
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-19 22:25:55 -0500

    update

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -311,3 +311,89 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
 */
+
+// // WORKS CORRECTLY
+// const Car = function (make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//     this.speed += 10;
+//     console.log(this.speed);
+// };
+
+// Car.prototype.brake = function () {
+//     this.speed -= 5;
+//     console.log(this.speed);
+// };
+
+// const EV = function (make, speed, charge) {
+//     Car.call(this, make, speed);
+//     this.charge = charge;
+// };
+
+// // Linking EV proto to Car proto
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//     this.charge = chargeTo;
+// };
+// EV.prototype.accelerate = function () {
+//     this.speed += 20;
+//     this.charge--;
+//     console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+// };
+
+// const tesla = new EV('Tesla', 120, 23);
+// tesla.brake();
+// tesla.accelerate();
+// tesla.chargeBattery(90);
+// tesla.accelerate();
+// // WORKS CORRECTLY
+
+//**********DISREGARD**********
+// class CarCl {
+//     constructor(make, speed) {
+//         this.make = make;
+//         this.speed = speed;
+//     }
+//     accelerate() {
+//         this.speed += 10;
+//         console.log(this.speed);
+//     }
+//     brake() {
+//         this.speed -= 5;
+//         console.log(this.speed);
+//     }
+
+//     get speedUS() {
+//         return this.speed / 1.6;
+//     }
+//     set speedUS(speed) {
+//         this.speed = speed * 1.6;
+//     }
+// }
+// works
+// class EV extends CarCl {
+//     constructor(make, speed, charge) {
+//         super(make, speed);
+//         this.charge = charge;
+//     }
+
+//     chargeBattery(chargeTo) {
+//         this.charge = chargeTo;
+//     }
+//     accelerate() {
+//         this.speed += 20;
+//         this.charge -= 1;
+//         console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+//     }
+// }
+// const tesla = new EV('Tesla', 120, 23);
+// tesla.brake();
+// tesla.accelerate();
+// tesla.chargeBattery(80);
+// tesla.accelerate();
+// works
+//**********DISREGARD**********
