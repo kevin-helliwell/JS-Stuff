d3209e7a2e5f308455b9c4fbd0778e32557238e4
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-02-25 14:51:23 -0500

    comment code cleaning

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -75,32 +75,3 @@ class App {
 }
 
 const app = new App();
-
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-
-//     // Clear input fields
-//     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-
-//     // Display marker
-//     console.log(this.mapEvent);
-//     const {lat, lng} = mapEvent.latlng;
-//     L.marker([lat, lng])
-//         .addTo(map)
-//         .bindPopup(
-//             L.popup({
-//                 maxWidth: 250,
-//                 minWidth: 100,
-//                 autoClose: false,
-//                 closeOnClick: false,
-//                 className: 'running-popup',
-//             })
-//         )
-//         .setPopupContent('Workout')
-//         .openPopup();
-// });
-
-// inputType.addEventListener('change', function () {
-//     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-//     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-// });
