21db05511e3abeee1b146c780f92858919805eb2
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-03-01 20:00:23 -0500

    local storage update

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -269,6 +269,14 @@ class App {
     _getLocalStorage() {
         const data = JSON.parse(localStorage.getItem('workouts'));
         console.log(data);
+
+        if (!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach((work) => {
+            this._renderWorkout(work);
+        });
     }
 }
 
