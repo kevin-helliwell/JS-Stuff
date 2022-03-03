d1cd9bc2894354b1f1f6eebef5f89a223262b275
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-03-02 21:19:31 -0500

    refactoring update

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -157,8 +157,19 @@ const getCountryAndNeighbour = function (country) {
 // Fetch and Promises example (REFACTORED)
 
 const getCountryData = function (country) {
-    fetch(`https://restcountries.com/v3/name/${country}`)
+    // Country 1
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
         .then(response => response.json())
-        .then(data => renderCountry(data[0]));
+        .then(data => {
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0];
+
+            if (!neighbour) return;
+
+            // Country 2
+            return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+        })
+        .then(response => response.json())
+        .then(data => renderCountry(data[0], 'neighbour'));
 };
 getCountryData('portugal');
