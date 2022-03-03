6f70d7532ed393b24475c2933bb91d62fd3f44bf
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-03-02 20:07:16 -0500

    "callback hell" example

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -116,3 +116,17 @@ const getCountryAndNeighbour = function (country) {
 
 // getCountryAndNeighbour('iraq');
 getCountryAndNeighbour('canada');
+
+// "Callback Hell"
+setTimeout(() => {
+    console.log('1 second passed');
+    setTimeout(() => {
+        console.log('2 seconds passed');
+        setTimeout(() => {
+            console.log('3 seconds passed');
+            setTimeout(() => {
+                console.log('4 seconds passed');
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
