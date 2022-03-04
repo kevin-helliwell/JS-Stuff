6cbf542058f619238c1626042793d04e6376db56
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-03-03 22:22:10 -0500

    promise combinator updates

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -584,7 +584,7 @@ const whereAmI = async function () {
 //     console.log('3: Finished getting location');
 // })();
 
-// Running promises in parallel
+// Running promises in parallel (promise combinator)
 const get3Countries = async function (c1, c2, c3) {
     try {
         // OLD WAY ->
@@ -606,4 +606,57 @@ const get3Countries = async function (c1, c2, c3) {
         console.error(err);
     }
 };
-get3Countries('portugal', 'canada', 'tanzania');
+// get3Countries('portugal', 'canada', 'tanzania');
+
+// Other promise combinators: race, allSettled and any
+
+// Promise.race
+// (async function () {
+//     const res = await Promise.race([
+//         getJSON(`https:restcountries.com/v3.1/name/italy`),
+//         getJSON(`https:restcountries.com/v3.1/name/egypt`),
+//         getJSON(`https:restcountries.com/v3.1/name/mexico`),
+//     ]);
+//     console.log(res[0]);
+// })();
+
+// const timeout = function (sec) {
+//     return new Promise(function (_, reject) {
+//         setTimeout(function () {
+//             reject(new Error('request took too long'));
+//         }, sec * 1000);
+//     });
+// };
+
+// Promise.race([getJSON(`https:restcountries.com/v3.1/name/tanzania`), timeout(5)])
+//     .then(res => console.log(res[0]))
+//     .catch(err => console.error(err));
+
+// // Promise.allSettled (ES2020)
+// Promise.allSettled([
+//     Promise.resolve('Success'),
+//     Promise.reject('ERROR'),
+//     Promise.resolve('Another success'),
+// ]).then(res => console.log(res));
+// // Outputs array of fulfilled/rejected promises + messages
+
+// // Promise.all
+// Promise.all([
+//     Promise.resolve('Success'),
+//     Promise.reject('ERROR'),
+//     Promise.resolve('Another success'),
+// ])
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+// // Errors out because of one reject
+
+// // Promise.any (ES2021)
+// Promise.any([
+//     Promise.resolve('Success'),
+//     Promise.reject('ERROR'),
+//     Promise.resolve('Another success'),
+// ])
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+// // Outputs 'SUCCESS' if at least one fulfilled promise
+// // **ignores rejected promises unless all reject**
