c22fcaf0101b35c917fc19963a508ef5c624735a
Kevin Helliwell <39539208+kevin-helliwell@users.noreply.github.com>
2022-03-04 17:55:00 -0500

    top-level await update

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -19,3 +19,26 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+// Top-level await (ES2022)
+// ** blocks execution of module until await finishes **
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return {title: data.at(-1).title, text: data.at(-1).body};
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
